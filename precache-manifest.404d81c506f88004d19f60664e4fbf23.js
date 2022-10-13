658d628e7970de85d70b53d9ec52b08cf6a1a027
steverova <71293149+steverova@users.noreply.github.com>
2022-10-13 17:12:45 -0600

    Updates

---
+++ precache-manifest.404d81c506f88004d19f60664e4fbf23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "800280e762582d357df5",
+    "url": "/frontend/static/css/main.3047b735.chunk.css"
+  },
+  {
+    "revision": "800280e762582d357df5",
+    "url": "/frontend/static/js/main.800280e7.chunk.js"
+  },
+  {
+    "revision": "03f83f65bab0c3e311a9",
+    "url": "/frontend/static/js/1.03f83f65.chunk.js"
+  },
+  {
+    "revision": "5cbbaf94823758d3c60e",
+    "url": "/frontend/static/js/runtime~main.5cbbaf94.js"
+  },
+  {
+    "revision": "cac746897f06aa8bd4d0b32074630706",
+    "url": "/frontend/index.html"
+  }
+];
