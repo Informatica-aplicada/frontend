2a6dac4110350ef8effd9cda88ae217b57c0bd4f
steverova <71293149+steverova@users.noreply.github.com>
2022-10-13 19:17:06 -0600

    Updates

---
+++ precache-manifest.3f10d0f436fae0314f3d6bf106757c0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c5e7abf773ba5d19976",
+    "url": "/frontend/static/css/main.257cdb1e.chunk.css"
+  },
+  {
+    "revision": "6c5e7abf773ba5d19976",
+    "url": "/frontend/static/js/main.6c5e7abf.chunk.js"
+  },
+  {
+    "revision": "03f83f65bab0c3e311a9",
+    "url": "/frontend/static/js/1.03f83f65.chunk.js"
+  },
+  {
+    "revision": "5cbbaf94823758d3c60e",
+    "url": "/frontend/static/js/runtime~main.5cbbaf94.js"
+  },
+  {
+    "revision": "023288fad4daf0da99c624a795939544",
+    "url": "/frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/frontend/precache-manifest.fb0bba7e25922781179b0c4db5ed3983.js"
+  "/frontend/precache-manifest.3f10d0f436fae0314f3d6bf106757c0b.js"
 );
 
 workbox.clientsClaim();
